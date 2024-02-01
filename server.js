099239bba4bf8cf90f99ea4bc5c0f4729a8d35b5
Lucy Kwon <ks02ks222@gmail.com>
2024-01-31 17:53:50 -0800

    change how to display all the roles

--- server.js
+++ server.js
@@ -12,18 +12,33 @@ const db = mysql.createConnection({
   database: "employees_db",
 });
 
-//function to update the query to view all the departments with its id
+//method to view all the departments
 const viewDepartments = async () => {
   const query_VD = `SELECT * FROM department`;
   try {
+    //ensures that the query returns a promise to use await and store the results in an array called departments 
     const [department] = await db.promise().query(query_VD);
-    //it returns all the records as objects
+    //it returns all the records as objects in an array 
     return [department];
   } catch (err) {
     console.error("Error Detected: ", err);
     return [];
   }
 };
+
+//method to view all the roles 
+const viewRoles = async () => {
+  const query_VR = `SELECT r.id AS id, r.title, d.name AS department, r.salary
+  FROM role AS r
+  JOIN department AS d ON r.department_id = d.id`;
+  try {
+    const [roles] = await db.promise().query(query_VR);
+    return [roles];
+  } catch (err) {
+    console.error("Error detected: " , err);
+    return [];
+  };
+}
 //function to start prompting a user what action they want to take
 const promptUser = async () => {
   //store the answer from the user prompt
@@ -66,16 +81,8 @@ LEFT JOIN employee AS m ON e.manager_id = m.id`;
       break;
     //when a user selects "View All Roles", update the query
     case "View All Roles":
-      query = `SELECT r.id AS id, r.title, d.name AS department, r.salary
-FROM role AS r
-JOIN department AS d ON r.department_id = d.id`;
-      //execute the query and fetch the results
-      db.query(query, function (err, results) {
-        if (err) {
-          console.error("Error detected: ", err);
-        }
-        console.table(results);
-      });
+      const roles = await viewRoles();
+      console.table(roles[0]);
       break;
     //when a user selects "View All Departments", call viewDepartments method.
     case "View All Departments":
